55181cb37a9d3c81ef100b6a25bad54d4628f312
Sujith BN <sujithbn01@gmail.com>
2026-04-24 19:19:19 +0530

    Added API to fetch all employess

--- ems-backend/src/main/java/com/sujith/ems_backend/service/EmployeeService.java
+++ ems-backend/src/main/java/com/sujith/ems_backend/service/EmployeeService.java
@@ -2,9 +2,13 @@ package com.sujith.ems_backend.service;
 
 import com.sujith.ems_backend.dto.EmployeeDTO;
 
+import java.util.List;
+
 public interface EmployeeService {
 
      EmployeeDTO addEmployee(EmployeeDTO employeeDTO);
 
     public EmployeeDTO getEmployee(Long id);
+
+    public List<EmployeeDTO> getALLEmployees();
 }
